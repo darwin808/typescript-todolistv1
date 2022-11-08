b2f95bd7ae28516d9cb3344914ecdf433ddd25be
darwin808 <darwin.apolinario12@gmail.com>
2022-11-08 22:54:04 +0800

    test

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 
 function Fetch() {
    const [collect, setcollect] = useState<any[]>([])
-   console.log(1234)
+   console.log(123)
    useEffect(() => {
       fetchUser()
    }, [])
