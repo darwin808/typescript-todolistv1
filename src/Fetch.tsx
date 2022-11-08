346b2f7e5633a0799fb531afedcf4b422428c8ec
darwin808 <darwin.apolinario12@gmail.com>
2022-11-08 22:52:38 +0800

    test

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -1,46 +1,40 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 function Fetch() {
-  const [collect, setcollect] = useState<any[]>([]);
-  useEffect(() => {
-    fetchUser();
-  }, []);
-  const fetchUser = async () => {
-    const data = await fetch("https://randomuser.me/api").then((e) => e.json());
+   const [collect, setcollect] = useState<any[]>([])
+   console.log(123)
+   useEffect(() => {
+      fetchUser()
+   }, [])
+   const fetchUser = async () => {
+      const data = await fetch("https://randomuser.me/api").then((e) => e.json())
 
-    // const data = await response.json();
+      // const data = await response.json();
 
-    const final = {
-      first: data.results[0].name.first,
-      last: data.results[0].name.last,
-    };
-    console.log(final);
+      const final = {
+         first: data.results[0].name.first,
+         last: data.results[0].name.last,
+      }
+      console.log(final)
 
-    //  await axios
-    //    .get("https://randomuser.me/ap1")
-    //   .then(function (response) {
-    //    console.log(response.data.results[0].name.first);
-    //    const final = {
-    //      first: response.data.results[0].name.first,
-    //      last: response.data.results[0].name.last,
-    //    };
+      //  await axios
+      //    .get("https://randomuser.me/ap1")
+      //   .then(function (response) {
+      //    console.log(response.data.results[0].name.first);
+      //    const final = {
+      //      first: response.data.results[0].name.first,
+      //      last: response.data.results[0].name.last,
+      //    };
 
-    //     console.log(final);
-    //   })
-    //   .catch(function (error) {
-    //   console.log(error);
-    //  });
-  };
+      //     console.log(final);
+      //   })
+      //   .catch(function (error) {
+      //   console.log(error);
+      //  });
+   }
 
-	return <div>
-
-
-		
-
-		
-		dad</div>;
-
+   return <div>dad</div>
 }
 
-export default Fetch;
+export default Fetch
